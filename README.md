83550cc724c7b9db55de945b113d7d3a5e1f555b
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2023-12-04 20:30:36 -0800

    Initial Release
    Thanks to Delta for testing and suggestions
    
    Co-Authored-By: Delta <7883807+xdelta@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # StresslessHeadless
- 
+
+A [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader) mod for [Resonite](https://resonite.com/) that reduces the resource usage for Headless clients by disabling frequent and heavy calculations.
+
+## What gets disabled:
+- VRIK
+- VRIKAvatar
+- DynamicBoneChain
+- EyeManager
+- Panner1D-4D
+- Spinner
+- Wiggler
+- Wobbler1D-4D
+- LookAt
+- LookAtUser
+- AutoLookAtUser
+- Bounding Boxes
+
+## Requirements
+- [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader)
+
+## Installation
+1. Install [ResoniteModLoader](https://github.com/resonite-modding-group/ResoniteModLoader).
+2. Place [StresslessHeadless.dll](https://github.com/Raidriar796/StresslessHeadless/releases/latest/download/StresslessHeadless.dll) into your `rml_mods` folder inside of the Headless installation. You can create it if it's missing, or if you launch the game once with ResoniteModLoader installed it will create the folder for you.
+3. Start the game. If you want to verify that the mod is working you can check your Resonite logs.
---
+++ StresslessHeadless/StresslessHeadless.cs
@@ -0,0 +1,193 @@
+﻿using HarmonyLib;
+using ResoniteModLoader;
+using FrooxEngine;
+using FrooxEngine.CommonAvatar;
+using FrooxEngine.FinalIK;
+
+namespace StresslessHeadless;
+
+public class StresslessHeadless : ResoniteMod
+{
+    public override string Name => "StresslessHeadless";
+    public override string Author => "Raidriar796";
+    public override string Version => "1.0.0";
+    public override string Link => "https://github.com/Raidriar796/StresslessHeadless";
+
+    public override void OnEngineInit()
+    {
+        Harmony harmony = new("net.raidriar796.StresslessHeadless");
+
+        if (Type.GetType("FrooxEngine.Headless.HeadlessCommands, Resonite") != null) harmony.PatchAll();
+    }
+
+    [HarmonyPatch(typeof(VRIK), "OnCommonUpdate")]
+    public class VRIKPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(VRIKAvatar), "OnCommonUpdate")]
+    public class VRIKAvatarPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(DynamicBoneChainManager), "Update")]
+    public class DynamicBoneChainPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(EyeManager), "OnCommonUpdate")]
+    public class EyeManagerPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Panner1D), "OnCommonUpdate")]
+    public class Panner1DPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Panner2D), "OnCommonUpdate")]
+    public class Panner2DPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Panner3D), "OnCommonUpdate")]
+    public class Panner3DPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Panner4D), "OnCommonUpdate")]
+    public class Panner4DPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Spinner), "OnCommonUpdate")]
+    public class SpinnerPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Wiggler), "OnCommonUpdate")]
+    public class WigglerPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Wobbler1D), "OnCommonUpdate")]
+    public class Wobbler1DPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Wobbler2D), "OnCommonUpdate")]
+    public class Wobbler2DPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Wobbler3D), "OnCommonUpdate")]
+    public class Wobbler3DPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Wobbler4D), "OnCommonUpdate")]
+    public class Wobbler4DPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(LookAt), "OnCommonUpdate")]
+    public class LookAtPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(LookAtUser), "OnCommonUpdate")]
+    public class LookAtUserPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(AutoLookAtUser), "OnCommonUpdate")]
+    public class AutoLookAtUserPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Bounds), "ComputeExactBounds")]
+    public class ComputeExactBoundsPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Bounds), "ForEachExactBoundedPoint")]
+    public class ForEachExactBoundedPointPatch
+    {
+        private static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
