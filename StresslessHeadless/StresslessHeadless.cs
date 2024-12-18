981694a9f6d5cd5bf6be9ccd09b7737a7e2375a2
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2024-12-18 03:30:18 -0800

    Updated version info
    Updated assembly and mod version info to 2.0.0-rc1

--- StresslessHeadless/StresslessHeadless.cs
+++ StresslessHeadless/StresslessHeadless.cs
@@ -8,7 +8,7 @@ public partial class StresslessHeadless : ResoniteMod
 {
     public override string Name => "StresslessHeadless";
     public override string Author => "Raidriar796";
-    public override string Version => "1.3.1";
+    public override string Version => "2.0.0-rc1";
     public override string Link => "https://github.com/Raidriar796/StresslessHeadless";
     private static Harmony harmony = new("net.raidriar796.StresslessHeadless");
 
