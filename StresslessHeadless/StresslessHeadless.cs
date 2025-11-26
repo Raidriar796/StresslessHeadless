17fc9ec3a4836a66f3a13271e89f3064a31a4214
Raidriar796 <145519075+Raidriar796@users.noreply.github.com>
2025-11-25 19:16:01 -0800

    net 10 hotfix
    Update to dotnet 10 and fix for a crash and exception on startup.

--- StresslessHeadless/StresslessHeadless.cs
+++ StresslessHeadless/StresslessHeadless.cs
@@ -8,7 +8,7 @@ public partial class StresslessHeadless : ResoniteMod
 {
     public override string Name => "StresslessHeadless";
     public override string Author => "Raidriar796";
-    public override string Version => "2.0.0";
+    public override string Version => "2.0.1";
     public override string Link => "https://github.com/Raidriar796/StresslessHeadless";
     private static Harmony harmony = new("net.raidriar796.StresslessHeadless");
 
@@ -17,17 +17,14 @@ public partial class StresslessHeadless : ResoniteMod
         Config = GetConfiguration()!;
         Config?.Save(true);
 
-        Engine.Current.RunPostInit(() =>
+        if (Config!.GetValue(Enable) && ModLoader.IsHeadless)
         {
-            if (Config!.GetValue(Enable) && ModLoader.IsHeadless)
-            {
-                SelectivePatches();
-                DelayedPatch();
-            }
-            else if (!ModLoader.IsHeadless)
-            {
-                Warn("StresslessHeadless is intended for headless clients only, please uninstall");
-            }
-        });
+            SelectivePatches();
+            DelayedPatch();
+        }
+        else if (!ModLoader.IsHeadless)
+        {
+            Warn("StresslessHeadless is intended for headless clients only, please uninstall");
+        }
     }
 }
